9a578ec3a149ed0ccdb48cebf0e18838cc35f2d8
david <davoodbahadori@gmail.com>
2025-03-20 17:27:41 +0330

    process message every 1 min

--- bot.py
+++ bot.py
@@ -110,6 +110,7 @@ class TelegramBot:
                     callback_query = update["callback_query"]
                     self.handle_callback_query(callback_query)
 
+
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
