e5d795a02c20642321beffe00d24b0235c44d687
david <davoodbahadori@gmail.com>
2025-03-20 18:03:07 +0330

    update test file

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import requests
-response = requests.get('https://api.telegram.org/bot8108600835:AAFcgvv4yd1-rk76qtdAUAgFMynd5vbhUkc/getMe', proxies={"http": None, "https": None})
-print(response.json())
+
+
+def test():
+    response = requests.get('https://api.telegram.org/bot8108600835:AAFcgvv4yd1-rk76qtdAUAgFMynd5vbhUkc/getMe', proxies={"http": None, "https": None})
+    print(response.json())
+
+if __name__ == "__main__":
+    test()
